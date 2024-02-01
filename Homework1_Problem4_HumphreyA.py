1228e286664f5d01c90f61ec6639483b273518cd
allieh1999 <157753511+allieh1999@users.noreply.github.com>
2024-02-01 16:05:25 -0500

    Add files via upload

---
+++ Homework1_Problem4_HumphreyA.py
@@ -0,0 +1,21 @@
+#Allie Humphrey and William Ebright
+
+#Homework 1: Problem 4
+
+#Maximum subarray: William Ebright
+
+#create a code version of the Maximum
+
+
+
+#Growth in execution: Allie Humphrey
+
+#Increases of two algorithms: Random list between [-1000, 1000] with an
+# increase of 100 sizes up to 10000, with a stepsize of 500
+#Look over the big O
+
+#will use ChatGPT as resource....
+
+
+
+#In excel, tab and put spaces in between once
